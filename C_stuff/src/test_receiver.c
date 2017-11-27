8fb6b9b64153ebf7a47e8fd3906cc5040fbe4d8d
sequae92 <spondon6102@gmail.com>
2017-11-27 00:00:19 -0500

    Added template functions in receiver

---
+++ C_stuff/src/test_receiver.c
@@ -0,0 +1,37 @@
+// This will contain some test functions for the receiver side
+// the functionality tested here will then be implemented in receiver.c
+
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+#define PORT 7735
+#define BUFFERSIZE 1500
+
+struct segmentACK{
+	char seqNum[32]; // sequence number of the packet
+	char dataACK[16]; // data field that is all zeroes
+	char packetType[16]; // 10101010101010 - indicating that is an ACK packet
+};
+
+int currseqNum; // this is a global value which keeps record of the current sequence number
+
+int calcuateChecksum(); //calculate the checksum of the incoming packet
+int dropPacket(float p); // drop packet according to a probability p - here p is between 0 and 1
+int main(int argc, char **argv){
+
+// socket should be open for receiving packets
+
+//drop packet according to the probability p which is read from the command line
+
+//once packet is received, read the fields inside the packet, calculate checksum
+// the function should check whether the packet is in sequence
+//if it is in sequence it sends an ACK segment
+// after that it writes the received data into a file
+// if the packet received is out of sequence, and ACK for the last received in sequence packet is sent
+// if the checksum is incorrect, the receiver does nothing
+
+
+}
