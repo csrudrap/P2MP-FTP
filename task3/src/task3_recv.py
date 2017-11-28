e5d2109e12a3d1d909a23ed6a1645bd6b3d19ae6
Chinmay <csrudrap@ncsu.edu>
2017-11-27 23:37:26 -0500

    Float issue

--- task3/src/task3_recv.py
+++ task3/src/task3_recv.py
@@ -2,6 +2,6 @@ import subprocess
 import os
 import time
 for j in range(5):
-    for i in range(0.01, 0.11, 0.01):
+    for i in range(1, 11, 1):
         time.sleep(2)
-        os.system("python receiver.py new_file.txt {} 65530".format(p))
+        os.system("python receiver.py new_file.txt {} 65530".format(float(p)/100))
