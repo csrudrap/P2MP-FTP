ee302234a0fe5a95cc70453eaa07e4b55d90037c
Chinmay <csrudrap@ncsu.edu>
2017-11-27 22:41:04 -0500

    File reorg

---
+++ task1/src/receiver.py
@@ -0,0 +1,165 @@
+# This will contain some test functions for the receiver side
+# the functionality tested here will then be implemented in receiver.c
+import random
+import struct
+import socket
+import os
+import sys
+
+cur_seq = -1 # is a global value which keeps record of the current seq num to prevent out-of-order packets.
+BUFFER_SIZE = 8192 # Arbitrarily chosen maximum limit.
+buf = ""
+
+
+def build_segment_ack(data):
+    seqNum = data[0]
+    dataACK = 0b0000000000000000
+    packetType = 0b1010101010101010
+    return struct.pack('iHH', seqNum, dataACK, packetType)
+
+
+def carry_around_add(a, b):
+    c = a + b
+    return (c & 0xffff) + (c >> 16)
+
+
+def verify_checksum(msg):
+    # Referred from stackoverflow.
+    return True
+    s = 0
+    for i in range(0, len(msg), 2):
+        w = ord(msg[i]) + (ord(msg[i+1]) << 8)
+        s = carry_around_add(s, w)
+    if (~s & 0xffff) == 0x0000:
+        return True
+    else:
+        return False
+
+
+#data = data.split()
+#data = map(lambda x: int(x,16), data)
+#data = struct.pack("%dB" % len(data), *data)
+
+#print ' '.join('%02X' % ord(x) for x in data)
+#print "Checksum: 0x%04x" % calculateChecksum(data)
+
+def dropSegment(data, p): # drop packet according to a probability p - here p is between 0 and 1
+    r = random.uniform(0, 1)
+    if r <= p:
+        print "Packet loss, sequence number = {}".format(data[0])
+        return True # drop packet
+    else:
+        return False # retain the packet
+
+
+def create_and_bind_socket(port):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    except Exception as e: # Change this line.
+        print "Socket could not be created", e
+        sys.exit(1)
+    if sock == None:
+        print "Socket is None! Exiting."
+        sys.exit(1)
+    else:
+        try:
+            sock.bind(('', port))
+        except Exception as e:
+            print e.message
+            print "Could not bind socket to port! Exiting"
+            shutdown_and_close(sock)
+            sys.exit(1)
+    return sock
+
+
+def shutdown_and_close(sock):
+    try:
+        sock.shutdown(socket.SHUT_RDWR)
+        print "Socket shutdown successfully."
+    except Exception as e:
+        print e.message, "Couldn't shutdown the socket on the server."
+    try:
+        sock.close()
+    except Exception as e:
+        print e.message, "Couldn't close the socket"
+
+
+def process_data(sock, raw_data, p, addr):
+    global cur_seq
+    # Header length is 4 + 2 + 2 = 8
+    n = len(raw_data) - 8
+    data = struct.unpack('iHH' + str(n) + 's', raw_data) # data is a tuple
+    if not dropSegment(data, p):
+        # Retain the segment, process it.
+        # Check if data has 4 fields at least.
+        print "SEQ RECVD IS: ", data[0]
+        if data[0] != cur_seq + 1:
+            print "Segment received not in sequence for seq number {}".format(data[0])
+            return False
+        else:
+            if not verify_checksum(data):
+                print "Checksum invalid, discarding segment for seq number {}".format(data[0])
+                return False
+            else:
+                append_to_file(data[3])
+                seg = build_segment_ack(data)
+                sock.sendto(build_segment_ack(data), addr)
+                cur_seq += 1
+                return True
+    else:
+        # Segment is discarded based on probability.
+        return False
+
+
+def append_to_file(data):
+    global buf
+    buf += data
+
+def ftp_recv(port, p):
+    sock = create_and_bind_socket(port)
+    while True:
+        # BUFFER_SIZE is the maximum limit on the size of the segment that the server will receive.
+        data, addr = sock.recvfrom(BUFFER_SIZE)
+        # print "Got connection from: %s", addr
+        if not data:
+            continue
+        #print "Received data:%s", data
+        ret = process_data(sock, data, p, addr)
+        # ret is True only when the segment is processed correctly and ACK is sent out.
+        if is_last_segment(data) and ret:
+            # After last segment is CORRECTLY processed, break out of the while True loop.
+            break
+        
+
+def is_last_segment(data):
+    n = len(data) - 8
+    unpacked_data = struct.unpack('iHH' + str(n) + 's', data) # unpacked_data is a tuple
+    data_id = unpacked_data[2]
+    # We identify the last segment by 0101010111111111.
+    return data_id == 0b0101010111111111
+
+
+#once packet is received, read the fields inside the packet, calculate checksum
+# the function should check whether the packet is in sequence
+#if it is in sequence it sends an ACK segment
+# after that it writes the received data into a file
+# if the packet received is out of sequence, and ACK for the last received in sequence packet is sent
+# if the checksum is incorrect, the receiver does nothing
+
+
+def main():
+    if len(sys.argv) < 4:
+        print "Please input 4 arguments: python receiver.py <FILENAME: STRING> <PROBABILITY: FLOAT BETWEEN 0 and 1> <PORT_NUM: INT>"
+        sys.exit(1)
+    filename = "../files/" + sys.argv[1]
+    p = float(sys.argv[2])
+    port = int(sys.argv[3])
+    global cur_seq
+    cur_seq = -1
+    ftp_recv(port, p)
+    with open(filename, "w") as f:
+        f.write(buf)
+    
+
+if __name__ == "__main__":
+    main()
---
+++ task3/src/sender.py
@@ -0,0 +1,194 @@
+import socket
+import sys
+import struct
+import threading
+from threading import Timer
+
+receivers = {}
+mss = 0
+buf = ""
+seg_type = 0b0101010100000000
+last_seg_type = 0b0101010111111111 
+timer = None
+timer_expired = True
+seq = 0
+ack_identifier = 0b1010101010101010
+port_num = 65530
+
+
+def calculate_checksum():
+    # calculate checksum on buff
+    return 0b1010110101110100   # Remove this line and return something valid.
+
+
+def create_socket_and_connect():
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    except Exception as e:
+        print "Socket could not be created"
+        sys.exit(1)
+    sock.settimeout(0.1)
+    return sock
+
+
+def ftp_init(ips):
+    global receivers
+    global timer
+    global timer_expired
+    global seq
+    for i in ips:
+        receivers[i] = False
+    buf = ""
+    timer_expired = True
+    timer = Timer(0.8, update_timer)
+    if timer.is_alive():
+        timer.cancel()
+    seq = 0
+
+
+def build_segment(is_last_byte):
+    global receivers
+    global timer
+    global timer_expired
+    global last_seg_type
+    global seg_type
+    
+    data_identifier = last_seg_type if is_last_byte else seg_type
+    checksum = calculate_checksum()
+    return struct.pack('iHH' + str(len(buf)) + 's', seq, checksum, data_identifier, buf)
+
+
+def stop_and_wait_worker(ip, is_last_byte):
+    # Send the segment to the receiver. 
+    # If data is not received, keep trying while timer_expired is false.
+    # If ack is incorrectly received, do not update the receiver dictionary, simply return.
+     
+    global timer_expired
+    global receivers
+    global port_num
+    data_received = False
+    sock = create_socket_and_connect()
+    sock.sendto(build_segment(is_last_byte), (ip, port_num))
+    while timer_expired == False and data_received == False:
+        try:
+            ack = sock.recvfrom(4096)
+            if ack is not None:
+                data_received = True
+            unpacked_ack = struct.unpack('iHH', ack[0])
+            if is_ack_correctly_received(unpacked_ack):  # Check if the ack received is valid.
+                receivers[ip] = True
+        except Exception as e:
+            pass    # Socket timeout may occur. Go back to the while loop without printing anything.
+     
+
+def is_ack_correctly_received(ack):
+    # Ack is correctly received if the identifier is 1010101000000000.
+    # ack is of type tuple
+    if len(ack) < 3:
+        return False
+    return ack[2] == ack_identifier
+
+
+def send_data(is_last_byte):
+    # Start timer
+    # Spawn threads for each IP in receivers.
+    # Wait for each thread to finish, call join.
+    global timer
+    global receivers
+    global timer_expired
+    if timer.is_alive():
+        timer.cancel()
+    timer = Timer(0.8, update_timer)
+    timer.start()
+    timer_expired = False
+    recv_threads = []
+    for i in range(len(receivers.keys())):
+        if receivers[receivers.keys()[i]] == False:
+            new_thread = threading.Thread(target=stop_and_wait_worker, args=(receivers.keys()[i], is_last_byte,))
+            new_thread.start()
+            recv_threads.append(new_thread)
+    for i in recv_threads:
+        i.join()
+    for i in receivers.keys():
+    # If any of the receivers haven't sent ACKs yet, return False so that this function can be called again.
+        if receivers[i] == False:
+            return False
+    if timer.is_alive():
+        timer.cancel()
+        timer = Timer(0.8, update_timer)
+    return True
+
+
+# To-Do: Doc string.
+# data: String, is_last_byte: boolean
+def stop_and_wait(data, is_last_byte):
+    global buf
+    global mss
+    global seq
+    buf += data
+    if len(buf) == mss or is_last_byte:
+        # Buffer is fully filled up or this is the last byte. In the latter case, send although
+        # buf size is less than mss
+        # Send the buffer over to the receivers
+        current_segment_done = False
+        while current_segment_done == False:
+            current_segment_done = send_data(is_last_byte)
+        if seq == 4294967295:   # Value of 2 to the power of 32.
+            seq = 0
+        else:
+            seq += 1
+        prepare_next_segment()
+                
+
+def prepare_next_segment():
+    global buf
+    global timer
+    global receivers
+    buf = ""
+    if timer.is_alive():
+        timer.cancel()
+    timer = Timer(0.8, update_timer)
+    for i in receivers.keys():
+        receivers[i] = False
+       
+
+def update_timer():
+    # set global timer_expired value to True. 
+    global timer_expired
+    global seq
+    timer_expired = True
+    print "Timeout, sequence number = {}".format(seq)
+
+
+# file_contents: String, ips: list of IP addresses as strings.
+def rdt_send(file_contents, ips): 
+    ftp_init(ips)
+    for i in range(len(file_contents)):
+        if i == len(file_contents) - 1:
+            # This is the last byte to be sent.
+            stop_and_wait(file_contents[i], True)
+        else:
+            stop_and_wait(file_contents[i], False)
+
+
+def main():
+    if len(sys.argv) < 4:
+        print "Please input 4 arguments: python sender.py <FILENAME: STRING> <MSS: INT> <PORT_NUM: INT>"
+        sys.exit(1)
+    if not sys.argv[1] or '../' in sys.argv[1]:
+        print "Please input the filename correctly."
+        sys.exit(1)
+    f = open("../files/" + sys.argv[1], "r")
+    file_contents = f.read()
+    ips = open("../files/ips.txt", "r").read().split('\n')[:-1]
+    global mss
+    mss = int(sys.argv[2])
+    global port_num
+    port_num = int(sys.argv[3])
+    rdt_send(file_contents, ips)
+    
+    
+if __name__ == "__main__":
+    main()
+
+
