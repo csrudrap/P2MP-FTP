b3620e7e3117bcbcc22569c4acca45114dd8f394
Chinmay <csrudrap@ncsu.edu>
2017-11-27 23:38:45 -0500

    Float issue - 2

--- task3/src/task3_recv.py
+++ task3/src/task3_recv.py
@@ -4,4 +4,4 @@ import time
 for j in range(5):
     for i in range(1, 11, 1):
         time.sleep(2)
-        os.system("python receiver.py new_file.txt {} 65530".format(float(p)/100))
+        os.system("python receiver.py new_file.txt {} 65530".format(float(i)/100))
